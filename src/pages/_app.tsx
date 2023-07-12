4cf596b62c04122a878bf93e985a81d6e94fe8c9
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-12 01:55:50 -0300

    feat: configured the header in _app because it is a header that belongs to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { getCssText } from "@/styles";
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "@/styles/pages/app";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -8,11 +11,19 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
+//colocar fora do myApp para renderizar somente um unica vez.
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-      <Component {...pageProps} />
+      <Container>
+        <Header>
+          <img src={logoImg.src} alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
     </main>
   );
 }
