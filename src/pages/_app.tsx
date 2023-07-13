9cfe89c1e3b76939b21827546359011065fcecf0
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-13 19:38:42 -0300

    feat: developed the styling of the product cart component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,20 @@ import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
 import logoImg from "../assets/logo.svg";
-import { Container, Header } from "@/styles/pages/app";
+import {
+  CartContainer,
+  CartList,
+  CartMain,
+  CartProduct,
+  CartProductInfo,
+  CartResume,
+  Container,
+  Header,
+  HeaderIcon,
+} from "@/styles/pages/app";
 import Image from "next/image";
+import { Handbag, X } from "@phosphor-icons/react";
+import { useState } from "react";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -15,12 +27,54 @@ const roboto = Roboto({
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const products = [1, 2, 3];
+  const [cartState, setCartState] = useState(false);
+
+  function handleChangeCartState() {
+    setCartState((state) => !state);
+  }
+
   return (
     <main className={roboto.className}>
       <Container>
         <Header>
           <Image src={logoImg} alt="" />
+          <HeaderIcon onClick={handleChangeCartState}>
+            <Handbag size={24} />
+            {products.length && <span>{products.length}</span>}
+          </HeaderIcon>
         </Header>
+        {cartState && (
+          <CartMain>
+            <CartContainer>
+              <button onClick={handleChangeCartState}>
+                <X size={24} />
+              </button>
+              <h3>Sacola de compras</h3>
+              <CartList>
+                <CartProduct>
+                  <Image src={logoImg} alt="" />
+                  <CartProductInfo>
+                    <p>Camiseta Beyond the Limits</p>
+                    <strong>R$ 79,90</strong>
+                    <button>Remover</button>
+                  </CartProductInfo>
+                </CartProduct>
+              </CartList>
+            </CartContainer>
+            <CartResume>
+              <div>
+                <p>Quantidade</p>
+                <p>3 itens</p>
+              </div>
+              <div>
+                <strong>Valor total</strong>
+                <strong>R$ 270,00</strong>
+              </div>
+              <button>Finalizar Compra</button>
+            </CartResume>
+          </CartMain>
+        )}
         <Component {...pageProps} />
       </Container>
     </main>
--- styles/pages/app.ts
+++ styles/pages/app.ts
@@ -13,4 +13,149 @@ export const Header = styled("header", {
   width: "100%",
   maxWidth: 1100,
   margin: "0 auto",
+
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export const HeaderIcon = styled("div", {
+  padding: "0.75rem",
+  background: "$gray800",
+  borderRadius: 8,
+  cursor: "pointer",
+  position: "relative",
+  top: "10px",
+  right: "10px",
+
+  span: {
+    position: "absolute",
+    top: "-10px",
+    right: "-10px",
+    fontSize: "0.875rem",
+    background: "$green300",
+    padding: "0.25rem 0.5rem",
+    borderRadius: 999,
+    boxShadow: "0 0 0 2px #121214",
+  },
+});
+
+export const CartMain = styled("main", {
+  position: "absolute",
+  top: 0,
+  right: 0,
+  width: "30rem",
+  height: "100vh",
+  background: "$gray800",
+  padding: "3rem",
+  zIndex: 1,
+});
+
+export const CartContainer = styled("div", {
+  h3: {
+    fontSize: "1.25rem",
+    fontWeight: "bold",
+  },
+
+  "& > button": {
+    position: "absolute",
+    top: 24,
+    right: 24,
+    border: 0,
+    background: "transparent",
+    color: "$gray300",
+    cursor: "pointer",
+  },
+
+  "& > h3": {
+    marginTop: "4.5rem",
+    marginBottom: "2rem",
+  },
+});
+
+export const CartList = styled("ul", {
+  height: "60vh",
+});
+
+export const CartProduct = styled("div", {
+  display: "flex",
+  gap: "1.25rem",
+
+  img: {
+    background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+    width: "6rem",
+    height: "6rem",
+  },
+});
+
+export const CartProductInfo = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+
+  p: {
+    fontSize: "1.125rem",
+    color: "$gray300",
+  },
+
+  strong: {
+    color: "$gray100",
+    fontSize: "1.125rem",
+    fontWeight: "bold",
+    lineHeight: 1.6,
+  },
+
+  button: {
+    border: 0,
+    background: "transparent",
+    color: "$green500",
+    fontSize: "1rem",
+    lineHeight: 1.6,
+    textAlign: "left",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+});
+
+export const CartResume = styled("div", {
+  position: "absolute",
+  bottom: 0,
+  right: 0,
+  marginBottom: "3rem",
+  marginRight: "3rem",
+
+  width: "24rem",
+
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.5rem",
+
+  "& > div": {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  strong: {
+    fontSize: "1.125rem",
+    fontWeight: "bold",
+    color: "$gray100",
+    lineHeight: 1.6,
+  },
+
+  button: {
+    marginTop: "3rem",
+    fontSize: "1.125rem",
+    fontWeight: "bold",
+    color: "$white",
+    padding: "1.25rem",
+    background: "$green300",
+    border: "none",
+    borderRadius: 8,
+    lineHeight: 1.6,
+    cursor: "pointer",
+    transition: "all 0.2s",
+
+    "&:hover": {
+      background: "$green500",
+    },
+  },
 });
