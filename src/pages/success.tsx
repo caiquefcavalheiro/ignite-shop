751f5b6dd2a796a0ffe517f731ee6c4153cf91d9
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-07-13 13:30:24 -0300

    feat: structured the success page and added styling

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,18 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <>
-      <h3>Página de sucesso</h3>
-    </>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+      <Link href="">Voltar ao catálogo</Link>
+    </SuccessContainer>
   );
 }
